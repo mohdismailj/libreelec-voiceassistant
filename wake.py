5f1afde1fe0024eecb71bfd3dd756bb526e4a236
mohdismailj <mohdismailj@gmail.com>
2021-02-24 17:13:26 +0530

    Update wake.py

--- wake.py
+++ wake.py
@@ -1,11 +1,13 @@
 import xbmc,xbmcgui 
 import os
-
-if xbmc.Player().isPlaying():
-	xbmc.executebuiltin('xbmc.PlayerControl(Play)')	
-
+def is_playback_paused():
+    return bool(xbmc.getCondVisibility("Player.Paused"))
+if not is_playback_paused():
+        xbmc.Player().pause()		
 dialog = xbmcgui.Dialog()
 notification = dialog.notification('iszaTV', 'Listening...')
 os.system("curl -X POST http://localhost:12101/api/listen-for-command")
 xbmc.executebuiltin('Dialog.Close(all,true)')
 xbmc.executebuiltin('xbmc.PlayerControl(Play)')
+if is_playback_paused():
+        xbmc.Player().pause()
