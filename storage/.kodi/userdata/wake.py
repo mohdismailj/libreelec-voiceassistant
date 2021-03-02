73fee89bb017171cf82dc6c4c682d6146b546b95
mohdismailj <mohdismailj@gmail.com>
2021-03-02 16:01:40 +0530

    Update and rename wake.py to storage/.kodi/userdata/wake.py

--- storage/.kodi/userdata/wake.py
+++ storage/.kodi/userdata/wake.py
@@ -5,9 +5,8 @@ def is_playback_paused():
 if not is_playback_paused():
         xbmc.Player().pause()		
 dialog = xbmcgui.Dialog()
-notification = dialog.notification('iszaTV', 'Listening...')
+notification = dialog.notification('iszaTV', 'Listening...',xbmcgui.NOTIFICATION_INFO,5000,True)
 os.system("curl -X POST http://localhost:12101/api/listen-for-command")
 xbmc.executebuiltin('Dialog.Close(all,true)')
-xbmc.executebuiltin('xbmc.PlayerControl(Play)')
 if is_playback_paused():
         xbmc.Player().pause()
